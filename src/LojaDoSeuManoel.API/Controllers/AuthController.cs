316115e8dd57ed4f7b60e0a3bc082ce3f8279d57
Lucas Rodrigues <lucas_luri@hotmail.com>
2025-05-28 23:05:07 -0300

    fix: aplicando DDD

--- src/LojaDoSeuManoel.API/Controllers/AuthController.cs
+++ src/LojaDoSeuManoel.API/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LojaDoSeuManoel.Application.DTOs.Request;
+using LojaDoSeuManoel.Application.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
@@ -9,45 +11,25 @@ namespace LojaDoSeuManoel.API.Controllers
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
-        private readonly IConfiguration _config;
+        private readonly AuthService _authService;
 
-        public AuthController(IConfiguration config)
+        public AuthController(AuthService authService)
         {
-            _config = config;
+            _authService = authService;
         }
 
         [HttpPost("login")]
         public IActionResult Login([FromBody] LoginRequest request)
         {
-            if (request.Usuario == "admin" && request.Senha == "123456") // Exemplo simples
-            {
-                var token = GerarTokenJWT();
+            if (request.Usuario == "admin" && request.Senha == "123456") {
+                var token = _authService.GerarTokenJWT();
                 return Ok(new { token });
             }
 
             return Unauthorized();
         }
-
-        private string GerarTokenJWT()
-        {
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-            var token = new JwtSecurityToken(
-                issuer: _config["Jwt:Issuer"],
-                audience: _config["Jwt:Audience"],
-                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_config["Jwt:ExpireMinutes"])),
-                signingCredentials: credentials
-            );
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
     }
 
-    public class LoginRequest
-    {
-        public string Usuario { get; set; }
-        public string Senha { get; set; }
-    }
+
 
 }
---
+++ src/LojaDoSeuManoel.Application/Services/AuthService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaDoSeuManoel.Application.Services
+{
+    public class AuthService
+    {
+        private readonly IConfiguration _config;
+
+        public AuthService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GerarTokenJWT()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_config["Jwt:ExpireMinutes"])),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
