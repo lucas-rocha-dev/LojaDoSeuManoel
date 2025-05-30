d18519a69800124f56c99646973f7ffa8bee2b8c
Lucas Rodrigues <lucas_luri@hotmail.com>
2025-05-29 23:26:54 -0300

    fix: Migração do Banco de dados

--- src/LojaDoSeuManoel.API/Program.cs
+++ src/LojaDoSeuManoel.API/Program.cs
@@ -66,10 +66,16 @@ builder.Services.AddAuthorization();
  
 var app = builder.Build();
 
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope()) {
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
 }
 
 app.UseHttpsRedirection(); 
